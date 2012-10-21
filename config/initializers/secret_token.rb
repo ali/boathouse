ce9b0f5bd0152d5073306226c33d0c2e14f049cf
Ali Ukani <ali.ukani@gmail.com>
2012-10-21 13:49:46 -0700

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boathouse::Application.config.secret_token = '80ce32ffe529f04d0306b3d761fd5c385c52bfe982004922777dd3ce23a04c852d6ad34c817c999beac861954fb3c3479d66637e26fc4cf33943faeeb64c7519'
